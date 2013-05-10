9a8aaea3e7b0c0dd444c1322904958c33e0ba915
Steven Benner <admin@stevenbenner.com>
2013-05-10 04:22:08 -0700

    Added Grunt automated linting process.

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+	'use strict';
+
+	// configure grunt
+	grunt.initConfig({
+		jshint: {
+			grunt: {
+				src: [ 'Gruntfile.js' ],
+				options: {
+					jshintrc: '.jshintrc'
+				}
+			},
+			js: {
+				src: [ 'js/*.js' ],
+				options: {
+					jshintrc: 'js/.jshintrc'
+				}
+			}
+		},
+		csslint: {
+			css: {
+				src: [ 'css/*.css' ],
+				options: {
+					'box-model': false,
+					'bulletproof-font-face': false,
+					'fallback-colors': false,
+					'ids': false,
+					'universal-selector': false
+				}
+			}
+		}
+	});
+
+	// load grunt plugins
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+
+	// register grunt tasks
+	grunt.registerTask('default', [ 'jshint', 'csslint' ]);
+};
